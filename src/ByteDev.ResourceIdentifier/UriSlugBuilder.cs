6cde4ad08e89fae9580875bbdd9ce165fa5fa553
Paul Stanley <paulstanley@protonmail.com>
2022-07-15 11:26:18 +0800

    Added xmldoc

--- src/ByteDev.ResourceIdentifier/UriSlugBuilder.cs
+++ src/ByteDev.ResourceIdentifier/UriSlugBuilder.cs
@@ -2,6 +2,10 @@
 
 namespace ByteDev.ResourceIdentifier
 {
+    /// <summary>
+    /// Represents a builder of URI slugs. A slug is usually used as the last part of a URI path
+    /// and if often a readable identifier.
+    /// </summary>
     public class UriSlugBuilder
     {
         private static readonly Random _random = new Random();
@@ -11,24 +15,46 @@ namespace ByteDev.ResourceIdentifier
         private string _suffixDateTime;
         private string _suffixRandom;
 
+        /// <summary>
+        /// Text to use in the slug.
+        /// </summary>
+        /// <param name="text">Slug text.</param>
+        /// <returns>Current builder instance.</returns>
         public UriSlugBuilder WithText(string text)
         {
             _text = text ?? string.Empty;
             return this;
         }
 
+        /// <summary>
+        /// Text to use in the slug.
+        /// </summary>
+        /// <param name="text">Slug text.</param>
+        /// <param name="maxLength">Max length of the text within the slug.</param>
+        /// <returns>Current builder instance.</returns>
         public UriSlugBuilder WithText(string text, int maxLength)
         {
             _text = text.SafeSubstring(0, maxLength);
             return this;
         }
 
+        /// <summary>
+        /// Character to use in place of space characters within the slug text.
+        /// Default is the hyphen character ('-').
+        /// </summary>
+        /// <param name="spaceChar">Character to replace space characters.</param>
+        /// <returns>Current builder instance.</returns>
         public UriSlugBuilder WithSpaceChar(char spaceChar)
         {
             _spaceChar = spaceChar;
             return this;
         }
 
+        /// <summary>
+        /// Appends a random string of characters as base 64 to the slug.
+        /// </summary>
+        /// <param name="length">Length of random characters before they are encoded as base 64.</param>
+        /// <returns>Current builder instance.</returns>
         public UriSlugBuilder WithRandomSuffix(int length)
         {
             if (length > 0)
@@ -37,12 +63,22 @@ namespace ByteDev.ResourceIdentifier
             return this;
         }
 
+        /// <summary>
+        /// Appends a date time encoded as base 64 to the slug.
+        /// </summary>
+        /// <param name="dateTime">Date time.</param>
+        /// <param name="format">Format to use when converting the date time to string (before encoding as base 64).</param>
+        /// <returns>Current builder instance.</returns>
         public UriSlugBuilder WithDateTimeSuffix(DateTime dateTime, string format = "yyyyMMddhhmmss")
         {
             _suffixDateTime = Base64ToUriSafe(dateTime.ToBase64String(format));
             return this;
         }
 
+        /// <summary>
+        /// Create a new slug string instance.
+        /// </summary>
+        /// <returns>New slug string instance.</returns>
         public string Build()
         {
             var slug = _text
